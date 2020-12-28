441bfe591d75983e73eb24589f3951efec0bb749
Vlad Kontsevykh <vladkontsevykh@list.ru>
2020-12-29 10:25:38 +1100

    Little extension for DataInputRow

--- src/main/kotlin/DataInputRow.kt
+++ src/main/kotlin/DataInputRow.kt
@@ -6,10 +6,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
 
 @Composable
-fun DataInputRow(modifier: Modifier = Modifier, name: String, curValue: String, singleLine: Boolean, onValueChange: (String) -> Unit) {
+fun DataInputRow(modifier: Modifier = Modifier, name: String, curValue: String, keyboardType: KeyboardType = KeyboardType.Text, singleLine: Boolean, onValueChange: (String) -> Unit) {
         TextField(curValue,
             modifier = modifier,
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+            keyboardOptions = KeyboardOptions(keyboardType = keyboardType),
             onValueChange = onValueChange,
             placeholder = { },
             singleLine = singleLine,
